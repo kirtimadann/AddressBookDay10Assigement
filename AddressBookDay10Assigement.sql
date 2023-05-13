c57d758832243f30ffd049ce6b2aa69149dc8689
lunainfotec <kirtimadan1997@gmail.com>
2023-05-13 12:00:58 +0530

    added : UC6

--- AddressBookDay10Assigement.sql
+++ AddressBookDay10Assigement.sql
@@ -7,3 +7,6 @@ describe AddressBook;
 insert into AddressBook(FirstName,LastName,Address,city,state,Zipcode,PhoneNumber,Email)values('kirti','madan','jalna','beed','MH','431203','96678987889','kirti@gmail.com'),('Aastha','rai','bihar','patna','up','431234','6789878895','Aastha@gmail.com');
 select * from AddressBook;
 DELETE FROM AddressBook WHERE FirstName='Aastha' and id = 2;
+select * from AddressBook;
+SELECT FirstName, city, state FROM AddressBook where FirstName = 'kirtusha'; 
+
